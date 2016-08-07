722d66b597f78bef51b1bc281433d2d18f576940
Jacob Paisley <jacobpaisley97@gmail.com>
2016-08-07 18:35:04 -0500

    Adding dependencies

--- index.js
+++ index.js
@@ -1,28 +1,47 @@
-var config = require('../config.json');
-var colors = require('colors/safe');
+var colors = require('colors');
 var fs = require('fs');
-var timestamp = require('./timestamp');
 
-module.exports = {
-	info: function(msg){
-		var tsText = timestamp.getTimestamp();
-		console.log(colors.grey(tsText) + ' [INFO] ' + msg);
-		writeToLog(tsText + ' [INFO] ' + msg);
-	},
-	warn: function(msg){
-		var tsText = timestamp.getTimestamp();
-		console.log(colors.grey(tsText) + colors.yellow(' [WARN] ' + msg));
-		writeToLog(tsText + ' [WARN] ' + msg);
-	},
-	err: function(msg){
-		var tsText = timestamp.getTimestamp();
-		console.log(colors.grey(tsText) + colors.red(' [ERR]  ' + msg));
-		writeToLog(tsText + ' [ERR]  ' + msg);
-	}
-};
+exports.info = function(msg){
+	var tsText = timestamp.getTimestamp();
+	console.log(colors.grey(tsText) + ' [INFO] ' + msg);
+	writeToLog(tsText + ' [INFO] ' + msg);
+}
+
+exports.warn = function(msg){
+	var tsText = timestamp.getTimestamp();
+	console.log(colors.grey(tsText) + colors.yellow(' [WARN] ' + msg));
+	writeToLog(tsText + ' [WARN] ' + msg);
+}
+
+exports.err = function(msg){
+	var tsText = timestamp.getTimestamp();
+	console.log(colors.grey(tsText) + colors.red(' [ERR]  ' + msg));
+	writeToLog(tsText + ' [ERR]  ' + msg);
+}
 
 function writeToLog(msg){
-	if(config.settings.enable_logs){
-			fs.appendFile('logs/' + timestamp.getLogStamp() + '.log', msg + '\n', function (err) {});
+	if(true){
+			fs.appendFile('logs/' + timestamp.getLogStamp() + '.log', msg + '\n', function(err){});
 	}
+}
+
+function getTimestamp(){
+  var now = new Date(),
+    	date = [now.getFullYear(), now.getMonth() + 1, now.getDate()],
+      time = [now.getHours(), now.getMinutes(), now.getSeconds()];
+
+	for (var i = 1; i < 3; i++) {
+	  if (time[i] < 10) {
+	    time[i] = "0" + time[i];
+	  }
+	}
+
+  return date.join("-") + " " + time.join(":");
+}
+
+function getLogStamp(){
+	var now = new Date(),
+	    date = [now.getFullYear(), now.getMonth() + 1, now.getDate()];
+
+	return date.join(".")
 }
